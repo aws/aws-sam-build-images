18950fc6cf0c2789f0fda2635bb07ec682110c39
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2021-08-16 12:58:07 -0700

    runtime: support for python3.9 (#21)
    * runtime: support for python3.9 (#3)
    
    * runtime: support for python3.9
    
    * Updated test for Python39
    
    * Updated license
    
    * Use public ECR for python3.9
    
    * Update the dockerfile
    
    Co-authored-by: jonife <79116465+jonife@users.noreply.github.com>

--- tests/test_build_images.py
+++ tests/test_build_images.py
@@ -161,6 +161,20 @@ class TestBIPython38(BuildImageBase):
         self.assertTrue(self.check_package_output("python --version", "Python 3.8."))
         self.assertTrue(self.is_package_present("pip"))
 
+class TestBIPython39(BuildImageBase):
+    __test__ = True
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass("python3.9", "Dockerfile-python39", "pip")
+
+    def test_packages(self):
+        """
+        Test packages specific to this build image
+        """
+        self.assertTrue(self.check_package_output("python --version", "Python 3.9."))
+        self.assertTrue(self.is_package_present("pip"))
+
 
 class TestBIRuby25(BuildImageBase):
     __test__ = True
