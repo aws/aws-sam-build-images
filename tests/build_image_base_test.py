53d16fb67ad2e17a778353eedf171f24ae1cb188
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2024-01-10 13:48:53 -0800

    Revert "debug maven issues"
    This reverts commit 39840b81a9647606eab32d91f94eba6a467483b1.

--- tests/build_image_base_test.py
+++ tests/build_image_base_test.py
@@ -116,16 +116,7 @@ class BuildImageBase(TestCase):
                 command=[
                     "/bin/sh",
                     "-c",
-                    sam_init + " && cd sam-app && sam build --debug",
-                ],
-            ).decode()
-            if self.runtime == "java8":
-                op = self.client.containers.run(
-                image=self.image,
-                command=[
-                    "/bin/sh",
-                    "-c",
-                    sam_init + " && cd sam-app && cd HelloWorldFunction && mvn clean install",
+                    sam_init + " && cd sam-app && sam build",
                 ],
             ).decode()
         self.assertTrue(op.find("Build Succeeded"))
