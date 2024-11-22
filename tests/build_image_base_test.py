64c5a3bf3476d7e4a95a9f6914240dc8f1d64994
Haresh Nasit <84355507+hnnasit@users.noreply.github.com>
2024-11-21 16:23:13 -0800

    add missing quotation mark

--- tests/build_image_base_test.py
+++ tests/build_image_base_test.py
@@ -162,7 +162,7 @@ class BuildImageBase(TestCase):
         # Skip this test for now as these checks are failing for x86
         # TODO: Add the checks back once the below docker issue is fixed
         # Error: 500 Server Error for : Internal Server Error ("failed to create shim task: OCI runtime create failed: runc create failed: unable to start container process: unable to apply cgroup configuration: cannot enter cgroupv2 "/sys/fs/cgroup/docker" with domain controllers -- it is in threaded mode: unknown")
-        if self.runtime in SKIP_CONTAINERIZED_BUILD_TESTS or self.tag == "x86_64:
+        if self.runtime in SKIP_CONTAINERIZED_BUILD_TESTS or self.tag == "x86_64":
             self.skipTest(f"Skipping for {self.runtime} and architecture: {self.tag}")
         init_args = [
             "sam",
