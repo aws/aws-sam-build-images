f7c54cd3ac7b2642b88694ea2bcc5a2de28a3c2b
Harold Sun <sunhua@amazon.com>
2025-11-07 19:28:51 +0000

    chore(test): fix java25 gradle test

--- tests/build_image_base_test.py
+++ tests/build_image_base_test.py
@@ -19,7 +19,7 @@ SKIP_CONTAINERIZED_BUILD_TESTS = {
 }
 # These are the runtimes which requires `--mount-with WRITE` option to build functions
 # in a containerized build
-MOUNT_WITH_WRITE_RUNTIMES = {"dotnet6", "dotnet8"}
+MOUNT_WITH_WRITE_RUNTIMES = {"dotnet6", "dotnet8", "java25"}
 
 
 class BuildImageBase(TestCase):
